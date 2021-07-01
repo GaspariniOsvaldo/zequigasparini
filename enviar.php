5c7aa258902e786e3467fc58b0170fdfd1efab6b
Gasparini Osvaldo <gaspariniosvaldo1@gmail.com>
2021-07-01 20:53:51 -0300

    correccion de php

--- enviar.php
+++ enviar.php
@@ -10,14 +10,14 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . $apellido . ",\r\n";
+$mensaje = "Este mensaje fue enviado por " . $nombre . $apellido . "\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Su telefono es: " . $celular . "\r\n";
+$mensaje .= "Su telefono es: " . $celular . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'gaspariniosvaldo1@gmail.com';
-$asunto = 'Mensaje de mi sitio web "Zequi Gasparini"';
+$asunto = 'Mensaje de mi sitio web Zequi Gasparini';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
